bdfff16af85e4e7adf314e01435a6ddcba6573e0
Kaisa Piipari <kaisa.piipari@gmail.com>
2018-10-31 14:15:16 +0000

    Module 4 WIP for backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
 
-module.exports = mutations;
+        return item;
+    }
+    // createDog(parent, args, ctx, info) {
+    //     // create a dog!
+    //     console.log(args)
+    // }
+};
+
+module.exports = Mutations;
